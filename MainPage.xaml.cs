a5512d02f7cbaab2bbd170804d2adddccec46b15
Alex Savin <2savinbuslov2@gmail.com>
2024-11-18 14:17:02 +0300

    initial commit
    with waste

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace MauiApp2
+{
+    /// <summary>
+    /// 52
+    /// </summary>
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string food = "cookie";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 2)
+                food = "cookies";
+
+            CounterBtn.Text = $"Eaten {count} " + food;
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# MauiApp2
+
+Nice app for antistress after hard day. 
+Clone of popular game "Humster combat".
+
+Easy app for testing new features in Sharp and using new design patterns and architectural approaches.
